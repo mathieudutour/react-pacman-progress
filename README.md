27de780bfc021e425d5525bce715492744b7b330
Mathieu Dutour <mathieu@dutour.me>
2015-08-12 10:52:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# react-progress-button
+
+> Simple [React](http://facebook.github.io/react/index.html) component for a fun prgress indicator.
+
+### [Demo](https://mathieudutour.github.io/react-pacman-progress)
+
+[![Demo](https://cdn.rawgit.com/mathieudutour/react-pacman-progress/master/example/demo.gif "Demo")](https://github.com/mathieudutour/react-pacman-progress/blob/master/example/index.html)
+
+## Install
+
+```bash
+npm install react-pacman-progress --save
+```
+
+or
+
+```bash
+bower install react-pacman-progress --save
+```
+
+## Example
+
+Controlled usage:
+
+```javascript
+var PacmanProgress = require('react-pacman-progress');
+
+var App = React.createClass({
+  getInitialState() {
+    return {currentIndex: 0};
+  },
+  render() {
+    var slides = [
+      {color: '#c0ffee'},
+      {color: '#deface'},
+      {color: '#0ff1ce'}
+    ]
+    return (
+      <div className="container" onCLick={this.handleClick}>
+        {
+          slides.map(function(slide, i) {
+            return <Slide index={i} currentIndex={this.state.currentIndex} color={slide.color} />;
+          }.bind(this))
+        }
+        <PacmanProgress items={slides.length} currentIndex={this.state.currentIndex} />
+      </div>
+    );
+  },
+
+  handleClick: function() {
+    this.setState({currentIndex: this.state.currentIndex + 1});
+  }
+});
+```
+
+## API
+
+### Props
+
+##### classNamespace
+
+Namespace for CSS classes, _optional_, default is `` i.e CSS classes are `'' + 'pacman'`.
+
+##### items
+
+Number of dots for pacman to eat. _required_
+
+##### currentIndex
+
+Position of the pacman. Start at 0. _required_
+
+##### onClick
+
+Function to call when clicking on a dot. _optional_, it takes 3 arguments:
+  * index of the dot clicked
+  * whether it was pacman which was clicked or not
+  * the event
+
+```
+handleClickPacman(index, isPacman, e) {
+  e.preventDefault();
+  this.setState({currentIndex: index});
+}
+```
+
+## Styles
+
+Look at [react-pacman-progress.css](https://github.com/mathieudutour/react-pacman-progress/blob/master/react-pacman-progress.css) for an idea on how to style this component.
+
+---
+
+MIT Licensed
---
+++ react-pacman-progress.js
@@ -0,0 +1,86 @@
+;(function (root, factory) {
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = factory(require('react'));
+  } else if (typeof define === 'function' && define.amd) {
+    define(['react'], factory);
+  } else {
+    root.PacmanProgress = factory(root.React);
+  }
+})(this, function componentFactory(React) {
+  var PacmanProgress = React.createClass({displayName: 'PacmanProgress',
+    propTypes: {
+      classNamespace: React.PropTypes.string,
+      items: React.PropTypes.number.isRequired,
+      currentIndex: React.PropTypes.number.isRequired,
+      onClick: React.PropTypes.func
+    },
+
+    getDefaultProps: function() {
+      return {
+        classNamespace: '',
+        onCLick: function() {}
+      };
+    },
+
+    getPacmanClass() {
+      return this.props.classNamespace +
+             'pacman ' +
+             this.props.classNamespace +
+             (this.props.currentIndex % 2 ? 'odd' : 'even');
+    },
+
+    getProgressClass() {
+      const className = [this.props.classNamespace + 'pacman-progress'];
+      if (this.props.currentIndex >= this.props.items) {
+        className.push(this.props.classNamespace + 'complete');
+      } else {
+        className.push(this.props.classNamespace + 'not-complete');
+      }
+      return className.join(' ');
+    },
+
+    getPointClass(i) {
+      const className = [this.props.classNamespace + 'point'];
+      if (this.props.currentIndex > i) {
+        className.push(this.props.classNamespace + 'done');
+      } else if (this.props.currentIndex === i) {
+        className.push(this.props.classNamespace + 'current');
+      } else {
+        className.push(this.props.classNamespace + 'not-done');
+      }
+      return className.join(' ');
+    },
+
+    getPointPosition(i) {
+      const position = {
+        top: '-5px',
+        left: (5 + 20 * (i - this.props.items / 2)) + 'px'
+      };
+      return position;
+    },
+
+    render: function() {
+      return (
+        React.createElement('div', {className: this.getProgressClass()},
+          React.createElement('div', {
+            className: this.getPacmanClass(),
+            style: this.getPointPosition(this.props.currentIndex),
+            onClick: this.props.onClick.bind(this, this.props.currentIndex, true)
+          }),
+            Array.apply(null, Array(this.props.items)).map(function(item, i) {
+              return (
+                React.createElement('div', {
+                  className: this.getPointClass(i),
+                  style: this.getPointPosition(i),
+                  key: 'pacman-progress-' + i,
+                  onClick: this.props.onClick.bind(this, i, false)
+                })
+              );
+            }.bind(this))
+        )
+      );
+    }
+  });
+
+  return PacmanProgress;
+});
